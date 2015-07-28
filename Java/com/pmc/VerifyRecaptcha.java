e451688ddb8c5b171cb09c862d864710c122e9b7
Trevor Sawler <trevor.sawler@gmail.com>
2015-07-28 09:27:46 -0300

    ReCaptcha

---
+++ Java/com/pmc/VerifyRecaptcha.java
@@ -0,0 +1,61 @@
+package com.pmc;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class VerifyRecaptcha {
+
+	public static final String url = "https://www.google.com/recaptcha/api/siteverify";
+	public static final String secret = "6Lfh_QkTAAAAAE0kNpHSspRmNS3XidfwEbmjEJVl";
+	private final static String USER_AGENT = "Mozilla/5.0";
+
+	public static boolean verify(String gRecaptchaResponse) throws IOException {
+		if (gRecaptchaResponse == null || "".equals(gRecaptchaResponse)) {
+			return false;
+		}
+
+		try {
+			URL obj = new URL(url);
+			HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+			// add request header
+			con.setRequestMethod("POST");
+			con.setRequestProperty("User-Agent", USER_AGENT);
+			con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+			String postParams = "secret=" + secret + "&response=" + gRecaptchaResponse;
+
+			// Send post request
+			con.setDoOutput(true);
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(postParams);
+			wr.flush();
+			wr.close();
+
+			int responseCode = con.getResponseCode();
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			// print result
+			String str1 = response.toString();
+
+			return str1.toLowerCase().contains("success");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+}
